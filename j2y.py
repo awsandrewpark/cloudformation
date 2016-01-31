13bb31a66233a80e165253e2055fd9dacd82c2e9
Pierre Jodouin <pjodouin@virtualcomputing.solutions>
2016-01-31 13:11:53 -0500

    Delete j2y.py

--- j2y.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import sys
-import json
-import yaml
-
-try:
-    print yaml.dump(yaml.load(json.dumps(json.loads(open(sys.argv[1]).read()))), default_flow_style=False, allow_unicode=True)
-except Exception as error:
-    print "Oops! Something was overlooked! {error_msg}".format(error_msg=str(error)) 
